ac9e3d0798a13a97126bede523210036017f78a2
ZhalgasB <zhalgas.bissenbai@narxoz.kz>
2026-02-13 21:35:19 +0500

    Character + Warrior classes

--- src/main/java/com/narxoz/rpg/character/Warrior.java
+++ src/main/java/com/narxoz/rpg/character/Warrior.java
@@ -1,21 +1,5 @@
 package com.narxoz.rpg.character;
 
-/**
- * Example concrete implementation of a Character.
- *
- * This is provided as a reference to help you get started.
- * Study this implementation, then create similar classes for Mage, Archer, etc.
- *
- * Notice:
- * - How attributes are initialized
- * - How methods are implemented
- * - The structure you should follow for other character types
- *
- * TODO: Create similar implementations for:
- * - Mage (high mana/intelligence, low health/strength)
- * - Archer (balanced stats, ranged combat)
- * - (Optional) Additional classes: Rogue, Paladin, etc.
- */
 public class Warrior implements Character {
 
     private String name;
@@ -24,25 +8,40 @@ public class Warrior implements Character {
     private int strength;
     private int intelligence;
 
-    // TODO: Add fields for equipped weapon and armor
-    // Think: Should Warrior know about its equipment?
-
 
     public Warrior(String name) {
         this.name = name;
-        // Warrior stats: high health and strength, low mana and intelligence
         this.health = 150;
         this.mana = 30;
         this.strength = 80;
         this.intelligence = 20;
     }
 
-    // TODO: Implement methods from Character interface
-    // You need to define those methods in Character interface first!
 
-    // Example method structure:
+
+
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int getHealth() {
+        return 150;
+    }
+
+    @Override
+    public int getMana() {
+        return 30;
+    }
+
+    @Override
+    public int getStrength() {
+        return 80;
+    }
+
+    @Override
+    public int getIntelligence() {
+        return 20;
     }
 
     public void displayStats() {
@@ -53,14 +52,8 @@ public class Warrior implements Character {
         System.out.println("Intelligence: " + intelligence);
     }
 
-    public void useSpecialAbility() {
-        System.out.println(name + " uses BERSERKER RAGE! Strength temporarily increased!");
+    public String useSpecialAbility() {
+        return " uses BERSERKER RAGE! Strength temporarily increased!";
     }
-
-    // TODO: Add equipment-related methods
-    // Examples:
-    // - void equipWeapon(Weapon weapon)
-    // - void equipArmor(Armor armor)
-    // - void displayEquipment()
 
 }
